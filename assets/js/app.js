f1302ef6052b63aed653e0ab7ccc95393b6114ce
Robin Boers <robindev2019@outlook.com>
2022-07-03 12:57:47 +0200

    Better loading bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,19 @@ import topbar from "../vendor/topbar"
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
-topbar.config({barColors: {0: "#0D9488"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({ barColors: { 0: "#0D9488" }, shadowColor: "rgba(0, 0, 0, .3)" })
+
+let topBarScheduled = undefined
+window.addEventListener("phx:page-loading-start", () => {
+    if (!topBarScheduled) {
+        topBarScheduled = setTimeout(() => topbar.show(), 120)
+    }
+})
+window.addEventListener("phx:page-loading-stop", () => {
+    clearTimeout(topBarScheduled);
+    topBarScheduled = undefined;
+    topbar.hide()
+})
 
 liveSocket.connect()
 
